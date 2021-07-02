9811213465dc40ee4aa5720477203de16a9e9d4d
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-07-02 16:13:47 +0200

    fix train

--- train.sh
+++ train.sh
@@ -3,108 +3,60 @@
 ### REBIAS
 mkdir -p checkpoints/rebias
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.999
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.999
 mv checkpoints/best.pth checkpoints/rebias/best0.999.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.997
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.997
 mv checkpoints/best.pth checkpoints/rebias/best0.997.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.995
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.995
 mv checkpoints/best.pth checkpoints/rebias/best0.995.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.99
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.99
 mv checkpoints/best.pth checkpoints/rebias/best0.990.pth
 
 
 ### Learned Mixin
 mkdir -p checkpoints/learned-mixin
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.999
-    --outer_criterion LearnedMixin
-    --g_lambda_inner 0
-    --n_g_pretrain_epochs 5
-    --n_g_update 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.999 --outer_criterion LearnedMixin --g_lambda_inner 0 --n_g_pretrain_epochs 5 --n_g_update 0
 mv checkpoints/best.pth checkpoints/learned-mixin/best0.999.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.997
-    --outer_criterion LearnedMixin
-    --g_lambda_inner 0
-    --n_g_pretrain_epochs 5
-    --n_g_update 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.997 --outer_criterion LearnedMixin --g_lambda_inner 0 --n_g_pretrain_epochs 5 --n_g_update 0
 mv checkpoints/best.pth checkpoints/learned-mixin/best0.997.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.995
-    --outer_criterion LearnedMixin
-    --g_lambda_inner 0
-    --n_g_pretrain_epochs 5
-    --n_g_update 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.995 --outer_criterion LearnedMixin --g_lambda_inner 0 --n_g_pretrain_epochs 5 --n_g_update 0
 mv checkpoints/best.pth checkpoints/learned-mixin/best0.995.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.99
-    --outer_criterion LearnedMixin
-    --g_lambda_inner 0
-    --n_g_pretrain_epochs 5
-    --n_g_update 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.99 --outer_criterion LearnedMixin --g_lambda_inner 0 --n_g_pretrain_epochs 5 --n_g_update 0
 mv checkpoints/best.pth checkpoints/learned-mixin/best0.990.pth
 
 ### RUBi
 mkdir -p checkpoints/rubi
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.999
-    --outer_criterion RUBi
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.999 --outer_criterion RUBi --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/rubi/best0.999.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.997
-    --outer_criterion RUBi
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.997 --outer_criterion RUBi --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/rubi/best0.997.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.995
-    --outer_criterion RUBi
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.995 --outer_criterion RUBi --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/rubi/best0.995.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.99
-    --outer_criterion RUBi
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.99 --outer_criterion RUBi --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/rubi/best0.990.pth
 
 ### VANILLA
 mkdir -p checkpoints/vanilla
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.999
-    --f_lambda_outer 0
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.999 --f_lambda_outer 0 --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/vanilla/best0.999.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.997
-    --f_lambda_outer 0
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.997 --f_lambda_outer 0 --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/vanilla/best0.997.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.995
-    --f_lambda_outer 0
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.995 --f_lambda_outer 0 --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/vanilla/best0.995.pth
 
-python main_biased_mnist.py --root /path/to/your/dataset
-    --train_correlation 0.99
-    --f_lambda_outer 0
-    --g_lambda_inner 0
+python main_biased_mnist.py --root ~/mnist --train_correlation 0.99 --f_lambda_outer 0 --g_lambda_inner 0
 mv checkpoints/best.pth checkpoints/vanilla/best0.990.pth
