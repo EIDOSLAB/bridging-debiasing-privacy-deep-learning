1b951b4dfc395f331fab2373d5176726db3609fc
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-07-04 20:49:07 +0200

    add sh script

---
+++ train_classifier.sh
@@ -0,0 +1,25 @@
+#!bin/sh
+
+# VANILLA
+python3 train_classifier.py --rho 0.999 --crit vanilla --root ~/mnist
+python3 train_classifier.py --rho 0.997 --crit vanilla --root ~/mnist
+python3 train_classifier.py --rho 0.995 --crit vanilla --root ~/mnist
+python3 train_classifier.py --rho 0.990 --crit vanilla --root ~/mnist
+
+# ReBias
+python3 train_classifier.py --rho 0.999 --crit rebias --root ~/mnist
+python3 train_classifier.py --rho 0.997 --crit rebias --root ~/mnist
+python3 train_classifier.py --rho 0.995 --crit rebias --root ~/mnist
+python3 train_classifier.py --rho 0.990 --crit rebias --root ~/mnist
+
+# rubi
+python3 train_classifier.py --rho 0.999 --crit rubi --root ~/mnist
+python3 train_classifier.py --rho 0.997 --crit rubi --root ~/mnist
+python3 train_classifier.py --rho 0.995 --crit rubi --root ~/mnist
+python3 train_classifier.py --rho 0.990 --crit rubi --root ~/mnist
+
+# learned-mixin
+python3 train_classifier.py --rho 0.999 --crit learned-mixin --root ~/mnist
+python3 train_classifier.py --rho 0.997 --crit learned-mixin --root ~/mnist
+python3 train_classifier.py --rho 0.995 --crit learned-mixin --root ~/mnist
+python3 train_classifier.py --rho 0.990 --crit learned-mixin --root ~/mnist
